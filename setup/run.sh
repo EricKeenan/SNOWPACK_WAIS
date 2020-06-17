387b15620fe8200b44e5ffdc3a857ee34be594d5
EricKeenan <eric.keenan@colorado.edu>
2020-06-17 11:53:27 -0600

    Update run.sh, CMakeCache files, and move bin and lib into snowpack directory

--- setup/run.sh
+++ setup/run.sh
@@ -1,93 +1,43 @@
 #!/bin/bash
 #This is the launching script for Alpine3D. Please make sure the user section matches your needs!
-########################### PBS directives. Edit for Sun Grid Engine runs
-#$ -m aes
-#$ -N urumqi
-#$ -S /bin/bash
-#$ -cwd
-#$ -j y
-#$ -l mem_free=12G ##nodes must have at least this much free memory
-##$ -pe threaded 2 ##at least 2 free cores per node
-#$ -pe smp 5
-##$ -pe orte 8 ##for MPI simulations
-########################### USER CONFIGURATION
-##either N or MPI, MPI_SGE or OPENMP
-PARALLEL=OPENMP
 
-##When using MPI (NOT MPI_SGE!), specify the machinefile you want to use. The machinefile format allows one
-##hostname per line followed optionally by a colon and the number of processes to run on that
-##host, e. g. 192.168.0.11:2 means: use 2 cores of the machine at address 192.168.0.11
-##use as many hosts as you deem reasonable, not defining a machinefile results in a local run.
-##Ultimately only NPROC many processes are used.
-MACHINEFILE=
-NPROC=8
+# Exit any conda environment
+conda deactivate 
 
-##when using openmp but NOT relying on Sun Grid Engine for job management, how many cores to use (otherwise it will be ignored)
-NCORES=1
+# Modules
+module purge
+ml intel; ml impi; ml proj; ml netcdf
 
-BEGIN="2014-10-01T01:00"
-END="2014-12-31T00:00"
+# Get simulation start and stop times
 source timespan.inc
 
-PROG_ROOTDIR=/media/nander/46d45ce3-2115-47b4-90e0-f01b0b010844/src/polarsnowpack_master/usr/bin/	#if you do not want to use Alpine3D from PATH, then point to where it is
-PROG_ROOTDIR=/projects/nawe3645/src/polarsnowpack_core/usr/bin/
-#should screen messages be redirected to stdouterr.log or printed directly on the screen?
-REDIRECT_LOGS=Y
-########################## END OF USER CONFIGURATION
+# Alpine-3D binary directory
+PROG_ROOTDIR=/scratch/summit/erke2265/SNOWPACK_WAIS/snowpack/usr/bin/
 
-#export DYLD_FALLBACK_LIBRARY_PATH=${PROG_ROOTDIR}:${DYLD_FALLBACK_LIBRARY_PATH}	#for osX
-#export LD_LIBRARY_PATH=${PROG_ROOTDIR}:${LD_LIBRARY_PATH}	#for Linux
-export LD_LIBRARY_PATH=${PROG_ROOTDIR}../lib/:${LD_LIBRARY_PATH}	#for Linux
+# Clear and create new output directory
+rm -rf ../output/grids
+rm -rf ../output/snofiles
+rm -rf ../outout/meteo
+
+mkdir -p ../output/grids
+mkdir -p ../output/snofiles
+mkdir -p ../output/meteo
+
+
+########################## END OF USER CONFIGURATION
+export LD_LIBRARY_PATH=/scratch/summit/erke2265/SNOWPACK_WAIS/snowpack/usr/lib:${LD_LIBRARY_PATH}
+
 EXE="${PROG_ROOTDIR}/alpine3d"
-if [ ! -f ${EXE} ]; then
-	EXE=`which alpine3d`
-fi
+
+
 N_EB=1
 N_SN=1
 
-#to combine OPENMP and MPI, run as MPI but with N_EB & N_SN > 1
-if [ "${PARALLEL}" == "MPI" ]; then
-	echo "Running with MPI"
-	MPIEXEC=${MPIEXEC:="mpiexec"}
-	MFILE=${MACHINEFILE:+"-machinefile ${MACHINEFILE}"}
-	EXE="${MPIEXEC} -n ${NPROC} ${MFILE} ${EXE}"
-	N_EB=1
-	N_SN=1
-elif [ "${PARALLEL}" == "MPI_SGE" ]; then
-	echo "Running with MPI under SGE"
-        MPIEXEC=${MPIEXEC:="mpiexec"}
-        EXE="${MPIEXEC} -np ${NSLOTS} ${EXE}"
-        N_EB=1
-        N_SN=1
-elif [ "${PARALLEL}" == "OPENMP" ]; then
-	echo "Running with OPENMP"
-	export OMP_NUM_THREADS=${NSLOTS:=$NCORES}
-	N_EB=$OMP_NUM_THREADS
-	N_SN=$OMP_NUM_THREADS
-else
-	echo "Running sequentially"
-fi
 
-##Below, always use a double pound sign for comments, otherwise SGE believes this is a job manager command and refuses to run
-##TOOL="valgrind --leak-check=full --show-reachable=yes --leak-resolution=high --undef-value-errors=yes --track-origins=yes --log-file=valgrind.log"
 TOOL="/usr/bin/time -v"
 
-A3D_CMD="${TOOL} ${EXE} \
+${TOOL} ${EXE} \
 --iofile=./io.ini \
 --enable-eb  \
---np-ebalance=${N_EB} \
---np-snowpack=${N_SN} \
---startdate=${BEGIN} --enddate=${END}"
+--startdate=${BEGIN} --enddate=${END}
 
-date
-if [[ ("${REDIRECT_LOGS}" == "Y") ||  ("${REDIRECT_LOGS}" == "y") ]]; then
-	${A3D_CMD} > stdouterr.log 2>&1 $*
-else
-	${A3D_CMD} 2>&1 $*
-fi
-ret=$?
-
-echo "Done Alpine3D Simulation. Return code=$ret"
-date
-echo
-exit $ret
