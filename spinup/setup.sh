b050791c1c0544c8e3a646e03002109dc828a9e3
EricKeenan <eric.keenan@colorado.edu>
2020-06-30 17:32:06 -0600

    End of day commit. Still need to finish spinup/setup.sh

--- spinup/setup.sh
+++ spinup/setup.sh
@@ -2,58 +2,30 @@
 
 # Misc tasks
 base_dir=$(pwd)
+ind0=1
+indf=20
 
-# Make simulation directories
-mkdir -p SP
-mkdir -p SP_high
-mkdir -p SP_low
-mkdir -p SP_warm
-mkdir -p SP_cold
-mkdir -p SP_more_wind
-mkdir -p SP_less_wind
-
-mkdir -p WAIS
-mkdir -p WAIS_high
-mkdir -p WAIS_low
-mkdir -p WAIS_warm
-mkdir -p WAIS_cold
-mkdir -p WAIS_more_wind
-mkdir -p WAIS_less_wind
-
-# Make input output directories
-for dir in *[^io]/ ; do
-	echo "Working on:  ${dir}"
-	if cd ${base_dir}/${dir} ; then
-		rm -r *
-		mkdir -p input
-		mkdir -p output
-		cp ../io/base.ini input/
-		# Copy input files into input directory
-		if [[ ${dir} = SP* ]] ; then # South Pole
-			cp ../io/SP.ini input/run.ini
-			cp ../io/SP.smet input/
-			cp ../io/SP.sno input/	
-		else # WAIS
-		       cp ../io/WAIS.ini input/run.ini
-		       cp ../io/WAIS.smet input/
-		       cp ../io/WAIS.sno input/
-		fi
-		# Modify forcing for ensemble
-		if [[ ${dir} == *_high/ ]]; then
-			sed -i "s/units_multiplier = 1 1 1 1 1 1 1 1 1.194 1/units_multiplier = 1 1 1 1 1 1 1.2 1 1.194 1/g" input/*.smet	
-		elif [[ ${dir} == *_low/ ]]; then
-                        sed -i "s/units_multiplier = 1 1 1 1 1 1 1 1 1.194 1/units_multiplier = 1 1 1 1 1 1 0.8 1 1.194 1/g" input/*.smet
-		elif [[ ${dir} == *_warm/ ]]; then
-                	sed -i "s/units_offset = 0 0 0 0 0 0 0 0 0 16.92/units_offset = 0 3.1 0 0 0 0 0 0 0 16.92/g" input/*.smet
-		elif [[ ${dir} == *_cold/ ]]; then
-                        sed -i "s/units_offset = 0 0 0 0 0 0 0 0 0 16.92/units_offset = 0 -3.1 0 0 0 0 0 0 0 16.92/g" input/*.smet
-                elif [[ ${dir} == *_more_wind/ ]]; then
-                        sed -i "s/units_offset = 0 0 0 0 0 0 0 0 0 16.92/units_offset = 0 0 0 2.4 0 0 0 0 0 16.92/g" input/*.smet
-                elif [[ ${dir} == *_less_wind/ ]]; then
-                        sed -i "s/units_offset = 0 0 0 0 0 0 0 0 0 16.92/units_offset = 0 0 0 -2.4 0 0 0 0 0 16.92/g" input/*.smet
-		fi 
+# Loop through each MERRA-2 grid cell inside of the MERRA-2 domain
+for site in $(seq ${ind0} ${indf}); do 
+	echo "Working on site ${site}"
+	
+	# Make site directory
+	cd ${base_dir}
+	site_dir=./io/site_${site}
+	mkdir -p ${site_dir}
+	
+	if cd ${site_dir}; then
+		# Clear and redifine site directory
+ 		rm -rf *
+	        mkdir -p input
+	        mkdir -p output
+		
+		# Copy input files (.sno, .ini) into input directory. Note that I will have deterimine T_s bottom. 
+		cp ../base.ini input/
+		cp ../../../input/meteo/A3D_site_${site}.smet input/
 	else
 		echo cd failed
 	fi
 done
+
 
