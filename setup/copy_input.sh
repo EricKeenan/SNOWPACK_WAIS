8541493cfbba6630907683dbcdbcf5ee3a961504
EricKeenan <eric.keenan@colorado.edu>
2020-07-07 16:41:07 -0600

    Update simulation to calculate SMB from A3D output .nc files

--- setup/copy_input.sh
+++ setup/copy_input.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 cp -r /pl/active/nasa_smb/A3D_WAIS_Input/* ../input/
+
+# Rename smet file (e.g. from VIR10.smet to A3D_site_10.smet)
+cd ../input/meteo/
+FILES="*"
+for f in $FILES
+do
+	mv $f A3D_site_${f: 3}
+done
+
