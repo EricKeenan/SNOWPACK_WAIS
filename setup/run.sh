2dd769a53246a0e038f0b12f2954c83ac7b588f2
EricKeenan <eric.keenan@colorado.edu>
2020-07-13 13:01:35 -0600

    Have a unified run.sh script which is called by job script

--- setup/run.sh
+++ setup/run.sh
@@ -1,43 +1,85 @@
 #!/bin/bash
-#This is the launching script for Alpine3D. Please make sure the user section matches your needs!
 
-# Exit any conda environment
-conda deactivate 
-
-# Modules
 module purge
 ml intel; ml impi; ml proj; ml netcdf
 
-# Get simulation start and stop times
-source timespan.inc
+# Are you restarting a previous Alpine-3D simulation? Yes (Y) No (N)
+RESTART=$1
 
-# Alpine-3D binary directory
-PROG_ROOTDIR=/scratch/summit/erke2265/SNOWPACK_WAIS/snowpack/usr/bin/
+REDIRECT_LOGS=N
 
-# Clear and create new output directory
-rm -rf ../output/grids
-rm -rf ../output/snofiles
-rm -rf ../outout/meteo
+# Add libraries to path
+export LD_LIBRARY_PATH=../snowpack/usr/lib:${LD_LIBRARY_PATH}
 
-mkdir -p ../output/grids
-mkdir -p ../output/snofiles
-mkdir -p ../output/meteo
+# Alpine-3D binary 
+EXE=$(pwd)/../snowpack/usr/bin/alpine3d
 
+if [ ! -f ${EXE} ]; then
+	EXE=`which alpine3d`
+fi
 
-########################## END OF USER CONFIGURATION
-export LD_LIBRARY_PATH=/scratch/summit/erke2265/SNOWPACK_WAIS/snowpack/usr/lib:${LD_LIBRARY_PATH}
-
-EXE="${PROG_ROOTDIR}/alpine3d"
-
-
-N_EB=1
-N_SN=1
-
+echo "Running with OPENMP"
+N_EB=$2
+N_SN=$2
 
 TOOL="/usr/bin/time -v"
 
-${TOOL} ${EXE} \
---iofile=./io.ini \
---enable-eb  \
---startdate=${BEGIN} --enddate=${END}
+if [[ ("${RESTART}" == "N") ]]; then
+	# Get simulation start and stop times
+	source timespan.inc
 
+	A3D_CMD="${TOOL} ${EXE} \
+	--iofile=./io.ini \
+	--enable-eb  \
+	--np-ebalance=${N_EB} \
+	--np-snowpack=${N_SN} \
+	--startdate=${BEGIN} --enddate=${END}"
+	
+	# Clear and create new output directory
+	rm -rf ../output/grids
+	rm -rf ../output/snofiles
+	rm -rf ../output/meteo
+
+	mkdir -p ../output/grids
+	mkdir -p ../output/snofiles
+	mkdir -p ../output/meteo
+
+else
+	# Get simulation start and stop times
+	cp timespan.inc restart_timespan.inc
+	file=$(ls ../output/snofiles/1_1_* | head -1)
+	line=$(sed -n '/ProfileDate/p' $file)
+	sed -i "s/1980-01-01T01:00/${line: 19}/g" restart_timespan.inc
+	source restart_timespan.inc
+
+        A3D_CMD="${TOOL} ${EXE} \
+        --iofile=./restart.ini \
+        --enable-eb  \
+	--restart \
+        --np-ebalance=${N_EB} \
+        --np-snowpack=${N_SN} \
+        --startdate=${BEGIN} --enddate=${END}"
+fi
+
+date
+if [[ ("${REDIRECT_LOGS}" == "Y") ||  ("${REDIRECT_LOGS}" == "y") ]]; then
+	${A3D_CMD} > ../output/log/stdouterr.log 2>&1 $*
+else
+	${A3D_CMD} 2>&1 $*
+fi
+ret=$?
+
+echo "Done Alpine3D Simulation. Return code=$ret"
+
+# Copy output to peta library
+d=$(date +%Y-%m-%d)
+mkdir -p /pl/active/nasa_smb/A3D_output/${d}/
+zip -r ../output/A3D_output.zip ../output/*
+cp ../output/A3D_output.zip /pl/active/nasa_smb/A3D_output/${d}/
+
+# Copy output .nc file to dropbox
+dbxcli put ../output/grids/a3d_grids.nc /Public/a3d_grids.nc
+
+date
+echo
+exit $ret
