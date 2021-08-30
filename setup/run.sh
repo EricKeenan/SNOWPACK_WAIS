d043af48325482b114f85db5da5e356b80a1c126
EricKeenan <eric.keenan@colorado.edu>
2021-08-30 11:01:21 -0600

    Configure A3D to run with MPI

--- setup/run.sh
+++ setup/run.sh
@@ -9,8 +9,11 @@ bash write_stn_lst.sh
 
 # Are you restarting a previous Alpine-3D simulation? Yes (Y) No (N)
 RESTART=$1
-num_nodes=$2
-num_threads_per_node=$3
+num_tasks=$2
+num_cpus_per_task=$3
+
+echo Tasks = ${num_tasks}
+echo CPU/Task = ${num_cpus_per_task}
 
 # Add libraries to path
 export LD_LIBRARY_PATH=../snowpack/usr/lib:${LD_LIBRARY_PATH}
@@ -24,13 +27,13 @@ if [ ! -f ${EXE} ]; then
 fi
 
 # Define the number of threads per node available for energy balance and SNOWPACK calculations
-N_EB=${num_threads_per_node}
-N_SN=${num_threads_per_node}
+N_EB=${num_cpus_per_task}
+N_SN=${num_cpus_per_task}
 
 # If nodes > 1, use MPI, else use OPENMP
-if (( ${num_nodes} > 1 )); then 
+if (( ${num_tasks} > 1 )); then
 	echo "Running with MPI"
-	TOOL="/usr/bin/time -v mpirun -v -np ${num_nodes}"
+	TOOL="/usr/bin/time -v mpirun -v"
 else
 	echo "Running with OPENMP"
 	TOOL="/usr/bin/time -v"
